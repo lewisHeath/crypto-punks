195bab70da34c1e6ee2ed0d64955c87cff581836
Lewis Heath <email@lewisheath.co.uk>
2022-02-13 00:16:51 +0000

    finished header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './Header.css'
 import punkLogo from '../assets/header/cryptopunk-logo.png'
 import searchIcon from '../assets/header/search.png'
+import themeSwitchIcon from '../assets/header/theme-switch.png'
 
 const Header = () => {
   return (
@@ -15,6 +16,22 @@ const Header = () => {
                 <img src={searchIcon} />
             </div>
             <input className='searchInput' placeholder='Collection, item or user...'/>
+        </div>
+
+        <div className='headerItems'>
+            <p>Drops</p>
+            <p>Marketplace</p>
+            <p>Create</p>
+        </div>
+
+        <div className='headerActions'>
+            <div className='themeSwitchContainer'>
+                <img src={themeSwitchIcon} alt="" />
+            </div>
+        </div>
+
+        <div className='loginButton'>
+            GET IN
         </div>
     </div>
   )
