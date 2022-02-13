183dc1a4e6b73c28e5ccfc2083c1da4ffb0aa2b2
Lewis Heath <email@lewisheath.co.uk>
2022-02-13 19:05:43 +0000

    finished cards

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import './App.css';
-import CollectionCard from './components/CollectionCard';
 import Header from './components/Header';
+import PunkList from './components/PunkList';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function App() {
 
+    const [punkListData, setPunklListData] = useState([]);
 
+    useEffect(() => {
+        const getMyNfs = async () => {
+            const openSeaData = await axios.get('https://testnets-api.opensea.io/assets?asset_contract_address=0x7847dbF9a57b4374D320A9c17dFb5D85741f2Abb&order_direction=asc');
+            console.log(openSeaData.data.assets);
+            setPunklListData(openSeaData.data.assets);
+        }
+
+        return getMyNfs()
+    }, [])
 
     return (
         <div className='app'>
             <Header />
-            <CollectionCard id={0} name={'Bandana Punk'} traits={[{'value': 7 }]} image='https://ipfs.thirdweb.com/ipfs/QmZ5fD3UTRh8ALZCpMdypHkhMQSXyi4yyCz3Ea19kPmtXg/0.jpg'/>
+            <PunkList punkListData={punkListData} />
         </div>
     );
 }
